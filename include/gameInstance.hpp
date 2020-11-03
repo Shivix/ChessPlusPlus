620ae52a08441142a1c14e55be7c982eb9a24e57
Mark Oborne <markoborne1@gmail.com>
2020-11-03 04:26:11 +0000

    moved implementation to source

--- include/gameInstance.hpp
+++ include/gameInstance.hpp
@@ -10,32 +10,9 @@ class gameInstance{
     sf::RenderWindow window{sf::VideoMode(800, 1000), "Chess++"};
     chessBoard board{};
 public:
-    void run(){
-        while(window.isOpen()){
-            sf::Event sfmlEvent{};
-            while(window.pollEvent(sfmlEvent)){
-                switch(sfmlEvent.type){
-                    case sf::Event::Closed:
-                        window.close();
-                        break;
-                }
-            }
-
-            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-
-                sf::Mouse::getPosition(window);
-            }
-            draw();
-        } 
-    }
-    void draw(){
-        window.clear();
-        for(auto&& i: board.squareArray){
-            window.draw(i);
-        }
-        window.draw(board.boardLine);
-        window.display(); 
-    }
+    void run();
+    
+    void draw();
 };
 
 
--- src/gameInstance.cpp
+++ src/gameInstance.cpp
@@ -1,1 +1,31 @@
 #include "../include/gameInstance.hpp"
+
+void gameInstance::run(){
+    while(window.isOpen()){
+        sf::Event sfmlEvent{};
+        while(window.pollEvent(sfmlEvent)){
+            switch(sfmlEvent.type){
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+            }
+        }
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+
+            sf::Mouse::getPosition(window);
+        }
+        draw();
+    }
+}
+
+void gameInstance::draw(){
+    window.clear();
+    for(auto&& i: board.squareArray){
+        window.draw(i);
+    }
+    window.draw(board.boardLine);
+    pawn testPawn{true};
+    window.draw(testPawn.sprite); // TODO: iterate through array of pieces?
+    window.display();
+}
