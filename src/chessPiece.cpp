4e3881cb82a6b64e5b3101d86bb906654394106c
Mark Oborne <markoborne1@gmail.com>
2020-11-03 07:15:22 +0000

    added comment

--- src/chessPiece.cpp
+++ src/chessPiece.cpp
@@ -10,12 +10,13 @@ chessPiece::chessPiece(enum colour colour):
 bool pawn::move() const{ // shouldnt need to return where it wants to move to it should already know.
     // piece doesnt care care if capture? so check for capture outside class?
 
+    // how to show available moves? store possible squares, move them with piece? 
+    
     // how to deal with illegal moves? move takes in requested new pos and checks? apply move func. return bool to show  if move valid?
 }
 
 pawn::pawn(enum colour colour):
-    chessPiece(colour)
-{
+    chessPiece(colour){
     if(colour == colour::white){
         spriteTexture.loadFromFile("../sprites/whitePawn.png");
     }
