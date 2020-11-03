e7532b7e1b656ea5f17eb2f8d2115f4fea9030d8
Mark Oborne <markoborne1@gmail.com>
2020-11-03 04:26:42 +0000

    moved implementation to source

--- include/chessBoard.hpp
+++ include/chessBoard.hpp
@@ -14,19 +14,7 @@ public:
     
     std::array<std::array<std::unique_ptr<chessPiece>, 8>, 8> board; //  2d array to represent the game board to track pieces
     
-    chessBoard(){
-        size_t k = 0;
-        for(size_t i = 0; i < 4; ++i){
-            for(size_t j = 0; j < 4; ++j){ // sets the size and position of the chessboard's squares // TODO: add ability to change colour
-                squareArray[k].setSize(sf::Vector2f(100, 100));
-                squareArray[k].setPosition(static_cast<float>(i) * 200, static_cast<float>(j) * 200);
-                squareArray[++k].setSize(sf::Vector2f(100, 100));
-                squareArray[k++].setPosition(static_cast<float>(i) * 200 + 100, static_cast<float>(j) * 200 + 100);
-            }
-        }
-        boardLine[0].position = sf::Vector2f(800, 800);
-        boardLine[1].position = sf::Vector2f(0, 800);
-    }
+    chessBoard();
     
 public:
     
--- src/chessBoard.cpp
+++ src/chessBoard.cpp
@@ -1,1 +1,15 @@
 #include "../include/chessBoard.hpp"
+
+chessBoard::chessBoard(){
+    size_t k = 0;
+    for(size_t i = 0; i < 4; ++i){
+        for(size_t j = 0; j < 4; ++j){ // sets the size and position of the chessboard's squares // TODO: add ability to change colour
+            squareArray[k].setSize(sf::Vector2f(100, 100));
+            squareArray[k].setPosition(static_cast<float>(i) * 200, static_cast<float>(j) * 200);
+            squareArray[++k].setSize(sf::Vector2f(100, 100));
+            squareArray[k++].setPosition(static_cast<float>(i) * 200 + 100, static_cast<float>(j) * 200 + 100);
+        }
+    }
+    boardLine[0].position = sf::Vector2f(800, 800);
+    boardLine[1].position = sf::Vector2f(0, 800);
+}
